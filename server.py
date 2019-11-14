debcb1ce987abd881a0f956e8e40ceee48cc618e
mogou <253883820@qq.com>
2019-11-14 13:15:40 +0800

    xiu gai server

--- server.py
+++ server.py
@@ -1,53 +1,126 @@
 import socket
 import mysql.MySQLDB
+import threading
+import time
+
+conneList = []
+sock = None
+lastpeopel_num = 0
+
+
+def recv_action():
+    global conneList
+    global lastpeopel_num
+
+    while True:
+        for client_socket, client_addr in conneList:
+            # client_socket.settimeout(10)
+            try:
+                buf = client_socket.recv(1024)
+                buf = bytes.decode(buf)
+                print(str(client_addr) + "  get value  " + buf)
+
+                if buf is not None or len(buf) != 0:
+                    for client_, addr_ in conneList:
+                        strvalue = "0002" + str(client_addr) + buf
+                        try:
+                            client_.send(bytes(strvalue, encoding="utf-8"))
+                        except ConnectionResetError:
+                            conneList.remove((client_, addr_))
+                            lastpeopel_num = 0
+                            pass
+                else:
+                    print("out")
+            except ConnectionAbortedError:
+                print("ConnectionAbortedError  player QZ exit")
+                conneList.remove((client_socket, client_addr))
+                print("remove player " + str(client_addr))
+                lastpeopel_num = 0
+                pass
+            except ConnectionResetError:
+                print("ConnectionResetError  player QZ exit")
+                conneList.remove((client_socket, client_addr))
+                print("remove player " + str(client_addr))
+                lastpeopel_num = 0
+                pass
+            except socket.timeout:
+                conneList.remove((client_socket, client_addr))
+                print("remove player " + str(client_addr))
+                lastpeopel_num = 0
+                pass
+            except Exception:
+                pass
+        time.sleep(0.1)
+
+
+def send_action():
+    global lastpeopel_num
+    global conneList
+    while True:
+        if lastpeopel_num != len(conneList) and len(conneList) != 0:
+            lastpeopel_num = len(conneList)
+            for client_socket, client_addr in conneList:
+                try:
+                    str_value = "0001" + str(len(conneList))
+                    client_socket.send(bytes(str_value, encoding="utf-8"))
+                except ConnectionAbortedError:
+                    print("ConnectionAbortedError  player QZ exit")
+                    conneList.remove((client_socket, client_addr))
+                    print("remove player " + str(client_addr))
+                    lastpeopel_num = 0
+                    pass
+                except ConnectionResetError:
+                    print("ConnectionResetError  player QZ exit")
+                    conneList.remove((client_socket, client_addr))
+                    print("remove player " + str(client_addr))
+                    lastpeopel_num = 0
+                    pass
+                except Exception:
+                    pass
+        time.sleep(0.1)
+
+
+def accept_action():
+    global conneList
+    global sock
+
+    while True:
+        try:
+            connection, address = sock.accept()
+            conneList.append((connection, address))
+            print(str(address) + "server lin num is " + str(len(conneList)))
+        except Exception:
+            pass
+        time.sleep(0.1)
 
 def listen_socket():
-
+    global conneList
+    global sock
+    global lastpeopel_num
     s = mysql.MySQLDB.DD()
     s.get_connect()
+    lastpeopel_num = 0
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind(('127.0.0.1', 8001))
     # sock.bind(('47.98.233.255', 8001))
     sock.listen(5)
     sock.setblocking(False)
-    print("server Start,Server_addr is 47.98.233.255,listen 8001,max link 5")
+    print("server Start Server_addr is 47.98.233.255 listen 8001 max link 5")
 
-    conneList = []
-    while True:
-        try:
-            # print("wati link ....")
-            connection, address = sock.accept()
-            conneList.append((connection, address))
-            print("server lin num is : " + str(len(conneList)))
-        except BlockingIOError:
-            pass
+    th_accept = threading.Thread(target=accept_action, args=(), name="accept_thread")
+    th_recv = threading.Thread(target=recv_action, args=(), name="recv_thread")
+    th_send = threading.Thread(target=send_action, args=(), name="send_thread")
+    th_accept.start()
+    th_recv.start()
+    th_send.start()
 
-        for client_socket, client_addr in conneList:
-            # client_socket.settimeout(10)
-            try:
-                buf = client_socket.recv(1024)
-                buf = bytes.decode(buf)
-                print(str(client_addr) + "  get value:  " + buf)
+    th_accept.join()
+    th_recv.join()
+    th_send.join()
+    sock.close()
 
-                if buf is not None:
-                    for client_, addr_ in conneList:
-                        strvalue = str(client_addr) + buf
-                        try:
-                            client_.send(bytes(strvalue, encoding="utf-8"))
-                        except ConnectionResetError:
-                            conneList.remove((client_, addr_))
-                else:
-                    print("out")
 
-            except ConnectionResetError:
-                print("player QZ exit")
-                conneList.remove((client_socket, client_addr))
-                print("remove player: " + str(client_addr))
-                pass
-            except socket.timeout:
-                conneList.remove((client_socket, client_addr))
-                print("remove player: " + str(client_addr))
-            except BlockingIOError:
-                pass
+
+
 
